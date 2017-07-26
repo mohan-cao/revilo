356d9c650fe6be3d2dcbddd6d818e38e5ff8ef3d
mohan-cao <mohancao@yahoo.com.au>
2017-07-26 13:52:43 +1200

    Added support for GraphStream library. Licensing added later.

--- src/main/java/revilo/App.java
+++ src/main/java/revilo/App.java
@@ -1,13 +1,21 @@
 package revilo;
 
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.implementations.SingleGraph;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main( String[] args ) {
+        Graph graph = new SingleGraph("Tutorial 1");
+        graph.addNode("A" );
+        graph.addNode("B" );
+        graph.addNode("C" );
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+        graph.display();
     }
 }
--- src/test/java/revilo/AppTest.java
+++ src/test/java/revilo/AppTest.java
@@ -1,38 +1,19 @@
 package revilo;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
+import static org.junit.Assert.*;
 
 /**
- * Unit test for simple App.
+ * Sample unit test
  */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
+public class AppTest {
 
     /**
-     * @return the suite of tests being tested
+     * NON-TRIVIAL TESTS WOW
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void App() {
+        assertTrue(true);
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
 }
