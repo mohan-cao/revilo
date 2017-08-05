202b9a03cb9d0db1d5d888c816d398619493ecf2
ashe848 <ashe848@aucklanduni.ac.nz>
2017-08-05 21:18:31 +1200

    Added descriptions for IDA* and BBA*

--- src/main/java/nz/co/revilo/Algorithms/BranchAndBoundAStar.java
+++ src/main/java/nz/co/revilo/Algorithms/BranchAndBoundAStar.java
@@ -1,5 +1,14 @@
 package nz.co.revilo.Algorithms;
 
+/**
+ * DFS with overestimate to begin with
+ * Uses list scheduling
+ * Progressively searches and updates the currently optimum (upper bound)
+ * Decrements bound, approaching the optimal solution from above
+ * 
+ * @author Abby S
+ *
+ */
 public class BranchAndBoundAStar {
 
 }
