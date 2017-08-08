f5775744aa12936359fbef74b9e01638d6eeb2fe
mohan-cao <mohancao@yahoo.com.au>
2017-08-08 12:32:14 +1200

    Basic smoke tests and App unit tests.

--- src/test/java/nz/co/revilo/AppTest.java
+++ src/test/java/nz/co/revilo/AppTest.java
@@ -1,8 +1,11 @@
 package nz.co.revilo;
 
 import org.junit.Test;
+import org.junit.experimental.categories.Category;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
 /**
  * Sample unit test
@@ -10,11 +13,47 @@ import static org.junit.Assert.assertTrue;
 public class AppTest {
 
     /**
-     * NON-TRIVIAL TESTS WOW
+     * Test for no valid params
      */
     @Test
-    public void App() {
-        assertTrue(true);
+    public void AppNoParams() {
+        try{
+            App.main(new String[]{});
+        }catch(RuntimeException re){
+            assertEquals(re.getMessage(),"Insufficient arguments given. Needs [input file] [# processors]");
+        }
     }
 
+    /**
+     * Test that a valid file was given to the application
+     */
+    @Test
+    public void AppFileError() {
+        try{
+            App.main(new String[]{"asdfghjkl-nice.dot","1"});
+        }catch(RuntimeException re){
+            assertEquals(re.getMessage(),"Input file does not exist");
+        }
+    }
+
+    /**
+     * Test that a valid file was given to the application
+     */
+    @Test
+    public void AppProcNumError() {
+        try{
+            App.main(new String[]{"input.dot","yes"});
+        }catch(RuntimeException re){
+            assertEquals(re.getMessage(),"Invalid number of processors");
+        }
+    }
+
+    /**
+     * SMOKE TEST. Runs automatically with Maven build. Disable with -DexcludedGroups=SlowTest.class
+     */
+    @Test
+    @Category(SlowTest.class)
+    public void AppSmokeTest(){
+        App.main(new String[]{"input.dot","1"});
+    }
 }
