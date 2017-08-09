4a8b705d02c7ffd87d483208ee3a0f905c50934b
mohan-cao <mohancao@yahoo.com.au>
2017-08-09 13:33:54 +1200

    Refactoring code in AppTest and simple algorithm test.

---
+++ src/test/java/nz/co/revilo/VeryBasicAlgorithmManagerTest.java
@@ -0,0 +1,30 @@
+package nz.co.revilo;
+
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class VeryBasicAlgorithmManagerTest {
+
+    /**
+     * Serial
+     */
+
+
+    /**
+     * One on each processor
+     */
+
+
+    /**
+     * Even split
+     */
+
+
+    /**
+     * No gaps
+     */
+}
