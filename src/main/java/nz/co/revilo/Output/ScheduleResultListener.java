e8b71660d434f5d66fcf14fe050d08cc918c93dc
abtagle <abtagle@gmail.com>
2017-08-10 18:10:13 +1200

    Added interface and finalSchedule method Javadoc

--- src/main/java/nz/co/revilo/Output/ScheduleResultListener.java
+++ src/main/java/nz/co/revilo/Output/ScheduleResultListener.java
@@ -2,7 +2,22 @@ package nz.co.revilo.Output;
 
 import java.util.List;
 
+/**
+ * Interface implemented by classes which need to make use of scheduling results. It allows those classes to receive
+ * key information about the schedule, particularly those in relation to producing output.
+ */
 public interface ScheduleResultListener {
+    /**
+     * Method which allows graph information abotu the final schedule to be passed to listeners.
+     *
+     * @param graphName
+     * @param nodeNames
+     * @param arcs
+     * @param arcWeights
+     * @param nodeWeights
+     * @param nodeStarts
+     * @param nodeProcessor
+     */
     public void finalSchedule(String graphName, List<String> nodeNames, List<List<Boolean>> arcs, List<List<Integer>> arcWeights, List<Integer> nodeWeights, List<Integer> nodeStarts, List<Integer> nodeProcessor); //TODO determine data structure to pass
 
 }
