95e666ff49f472f25f24985b2433fe221981f371
mkem114 <mkem114@aucklanduni.ac.nz>
2017-08-14 13:53:02 +1200

    Parameters Refactor
    Added and completed java doc comments

--- src/main/java/nz/co/revilo/CommandLine/Parameters.java
+++ src/main/java/nz/co/revilo/CommandLine/Parameters.java
@@ -12,25 +12,45 @@ import com.beust.jcommander.Parameter;
  */
 public class Parameters {
 
-    @Parameter(names={"--parallel", "-p"})
+    @Parameter(names = {"--parallel", "-p"})
     private int _parallelCores = 1; //default one core
-    @Parameter(names={"--visualise", "--visualize", "-v"})
+    @Parameter(names = {"--visualise", "--visualize", "-v"})
     private boolean _visualise = false;
-    @Parameter(names={"--output", "-o"})
+    @Parameter(names = {"--output", "-o"})
     private String _outputName = null; //should get input name actually
 
+    /**
+     * Gets the amount of cores to paralelise processing on
+     *
+     * @return the amount of cores
+     */
     public int getParallelCores() {
         return _parallelCores;
     }
 
+    /**
+     * Whether or not to visualise the process
+     *
+     * @return visualisation switch
+     */
     public boolean getVisualise() {
         return _visualise;
     }
 
+    /**
+     * The desired file output name
+     *
+     * @return filename for output filename
+     */
     public String getOutputName() {
         return _outputName;
     }
 
+    /**
+     * Sets the desired file output name
+     *
+     * @param outputFileName the name of the output file
+     */
     public void setDefaultOutputName(String outputFileName) {
         _outputName = outputFileName + "-output.dot";
     }
