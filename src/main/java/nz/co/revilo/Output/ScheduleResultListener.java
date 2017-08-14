441e6a0af0e249ca9c36113fa3e237514fe18b44
mkem114 <mkem114@aucklanduni.ac.nz>
2017-08-14 13:40:27 +1200

    ScheduleResultListener Refactor
    Updated commenting about function arguments

--- src/main/java/nz/co/revilo/Output/ScheduleResultListener.java
+++ src/main/java/nz/co/revilo/Output/ScheduleResultListener.java
@@ -6,19 +6,19 @@ import java.util.List;
  * Interface implemented by classes which need to make use of scheduling results. It allows those classes to receive
  * key information about the schedule, particularly those in relation to producing output.
  * @author Michael Kemp
- * @version Beta
+ * @version 1.0
  */
 public interface ScheduleResultListener {
     /**
-     * Method which allows graph information abotu the final schedule to be passed to listeners.
+     * Method which allows graph information about the final schedule to be passed to listeners.
      *
-     * @param graphName
-     * @param nodeNames
-     * @param arcs
-     * @param arcWeights
-     * @param nodeWeights
-     * @param nodeStarts
-     * @param nodeProcessor
+     * @param graphName     Name of graph
+     * @param nodeNames     Name of each task
+     * @param arcs          The existence of arcs
+     * @param arcWeights    How long to transfer results of task completion to another processor
+     * @param nodeWeights   How long each task takes
+     * @param nodeStarts    When each task starts
+     * @param nodeProcessor What processor each task is on
      */
     void finalSchedule(String graphName, List<String> nodeNames, List<List<Boolean>> arcs, List<List<Integer>> arcWeights, List<Integer> nodeWeights, List<Integer> nodeStarts, List<Integer> nodeProcessor); //TODO determine data structure to pass
 }
