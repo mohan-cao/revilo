b59b671dc5391a424a52bacde57dac8361934fd4
mkem114 <mkem114@aucklanduni.ac.nz>
2017-08-14 13:45:45 +1200

    DotFileWriter
    Updated the version tag
    Completed commenting

--- src/main/java/nz/co/revilo/Output/DotFileWriter.java
+++ src/main/java/nz/co/revilo/Output/DotFileWriter.java
@@ -18,21 +18,31 @@ public class DotFileWriter extends DotFileProducer {
      * @param output PrintWriter set up and used to create the new DOT file, and print output to it
      */
     protected void produceOutput(PrintWriter output) {
-        // Name and start
+        // Prints graph name
         String temp;
+        // If there's a name
         if (_graphName != null) {
+            // And it's longer than 1 character
             if (_graphName.length() > 1) {
+                // Capitalise the first character
                 temp = _graphName.substring(0, 1).toUpperCase() + _graphName.substring(1);
             } else {
+                // otherwise just capitalise the whole thing
                 temp = _graphName.toUpperCase();
             }
         } else {
+            // and if there isn't a name then it's empty
             temp = "";
         }
-
+        // Prints the graph name
         output.println("digraph \"output" + temp + "\" {");
 
-        // Arcs
+        // Prints nodes with their weights
+        for (int node = 0; node < _arcs.size(); node++) {
+            output.println("\t\t" + _nodeNames.get(node) + "\t\t[Weight=" + _nodeWeights.get(node) + ",Start=" + _nodeStarts.get(node) + ",Processor=" + _nodeProcessor.get(node) + "];");
+        }
+
+        // Prints arcs with their weights
         for (int from = 0; from < _arcs.size(); from++) {
             for (int to = 0; to < _arcs.get(from).size(); to++) {
                 if (_arcs.get(from).get(to)) {
@@ -41,12 +51,7 @@ public class DotFileWriter extends DotFileProducer {
             }
         }
 
-        // Nodes
-        for (int node = 0; node < _arcs.size(); node++) {
-            output.println("\t\t" + _nodeNames.get(node) + "\t\t[Weight=" + _nodeWeights.get(node) + ",Start=" + _nodeStarts.get(node) + ",Processor=" + _nodeProcessor.get(node) + "];");
-        }
-
-        // End
+        // Print the closing line
         output.println("}");
     }
 }
