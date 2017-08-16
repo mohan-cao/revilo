62a102273bc5413a6cd8d3ed50fec3b9ed390354
ashe848 <ashe848@aucklanduni.ac.nz>
2017-08-16 22:26:34 +1200

    First implementation of primitive interface BnB

--- src/main/java/nz/co/revilo/Scheduling/BranchAndBoundAlgorithmManager.java
+++ src/main/java/nz/co/revilo/Scheduling/BranchAndBoundAlgorithmManager.java
@@ -1,6 +1,7 @@
 package nz.co.revilo.Scheduling;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class BranchAndBoundAlgorithmManager extends AlgorithmManager {
@@ -12,6 +13,9 @@ public class BranchAndBoundAlgorithmManager extends AlgorithmManager {
 	int numNodes;
 	int totalNodeWeights;
 	private int upperBound;
+	private Schedule optimalSchedule;
+	private List<Integer> nodeStartTimes=new ArrayList<>();;
+	private List<Integer> nodeProcessors=new ArrayList<>();;
 
 	public BranchAndBoundAlgorithmManager(int processingCores) {
 		super(processingCores);
@@ -47,6 +51,25 @@ public class BranchAndBoundAlgorithmManager extends AlgorithmManager {
 		while(!rootSchedules.isEmpty()){
 			bnb(rootSchedules.remove(0));
 		}
+		
+		passResults();
+	}
+
+	private void passResults() {
+		for(int nodeId=0; nodeId<numNodes; nodeId++){
+			nodeStartTimes.add(optimalSchedule.closedSet.get(nodeId)._startTime);
+			nodeProcessors.add(optimalSchedule.closedSet.get(nodeId)._processor);
+		}	
+		
+		getListener().finalSchedule(
+                _graphName,
+                Arrays.asList(_nodeNames),
+                primToBool2D(_arcs),
+                primToInt2D(_arcWeights),
+                primToInt(_nodeWeights),
+                nodeStartTimes,
+                nodeProcessors
+        );
 	}
 
 	/**
@@ -55,7 +78,32 @@ public class BranchAndBoundAlgorithmManager extends AlgorithmManager {
 	 * @author Abby S
 	 */
 	private void bnb(Schedule s) {
-		
+		//TODO: not good enough
+		if(s.lowerBound>upperBound){
+			return; //break tree at this point
+		}
+
+		//found optimal for the root started with
+		if(s.openSet.isEmpty()){
+			//reached end of a valid schedule. Never broke off, so is optimal
+			optimalSchedule=s;
+			upperBound=s.finishTimes[0];
+			for(int i=1; i<_processingCores;i++){
+				if(s.finishTimes[i]>upperBound) upperBound=s.finishTimes[1];
+			}
+			return;
+		}
+
+		//continue DFS
+		List<Schedule> nextSchedules = new ArrayList<>();
+		for(int n:s.independentSet){
+			for(int p=0; p<_processingCores; p++){
+				nextSchedules.add(new Schedule(this, s, n, p));
+			}
+		}
+		for(Schedule nextSchedule:nextSchedules){
+			bnb(nextSchedule);
+		}
 	}
 
 	/**
@@ -82,4 +130,45 @@ public class BranchAndBoundAlgorithmManager extends AlgorithmManager {
 			}
 		}	
 	}
+	
+	
+    /**
+     * Creates an object type 2d bool list from primitive array
+     * @param prim the primitive boolean 2d array
+     * @return b the reference type list
+     */
+    private List<List<Boolean>> primToBool2D(boolean[][] prim) {
+        List<List<Boolean>> b = new ArrayList<>();
+        for (int i = 0; i < prim.length; i++) {
+            b.add(new ArrayList<>());
+            for (int j = 0; j < prim[i].length; j++) {
+                b.get(i).add(prim[i][j]);
+            }
+        }
+        return b;
+    }
+
+    /**
+     * Creates an object type 2d int list from primitive array
+     * @param prim the primitive int 2d array
+     * @return n the reference type list
+     */
+    private List<List<Integer>> primToInt2D(int[][] prim) {
+        List<List<Integer>> n = new ArrayList<>();
+        for (int i = 0; i < prim.length; i++) {
+            n.add(new ArrayList<>());
+            for (int j = 0; j < prim[i].length; j++) {
+                n.get(i).add(prim[i][j]);
+            }
+        }
+        return n;
+    }
+
+    private List<Integer> primToInt(int[] prim) {
+        ArrayList<Integer> n = new ArrayList<>();
+        for (int i: prim) {
+            n.add(i);
+        }
+        return n;
+    }
 }
