e10ac3c263e6b97b95209e86122bbe9c5cff90e8
mkem114 <mkem114@aucklanduni.ac.nz>
2017-08-23 13:19:18 +1200

    Batter test script
    Currently not working due to no manifest attribute but it should

--- batter.sh
+++ batter.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 start=$SECONDS
-java -cp . nz.co.revilo.App /test_inputs/Fork_Nodes_30_CCR_1.02_WeightType_Random.gxl 4 -o temp.dot -p 1
+java -jar ./target/revilo-task-scheduler-1.0-SNAPSHOT.jar ./test_inputs/Fork_Nodes_30_CCR_1.02_WeightType_Random.gxl 4 -o temp.dot -p 1
 duration=$(( SECONDS - start ))
+printf 'took %d seconds to run\n' $duration
