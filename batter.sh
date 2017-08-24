4adef7d7f5fa69ebbee99af7bbf72b1ebdcc4bac
mkem114 <mkem114@aucklanduni.ac.nz>
2017-08-25 02:04:56 +1200

    Updated bash batter file to run four tests and be able to compare optimal to what was found

--- batter.sh
+++ batter.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
+set -x #echo on
 start=$SECONDS
-java -jar ./target/revilo-task-scheduler-1.0-SNAPSHOT.jar ./test_inputs/Fork_Nodes_30_CCR_1.02_WeightType_Random.gxl 4 -o temp.dot -p 1
+java -jar ./target/revilo-task-scheduler-1.0-SNAPSHOT.jar ./test_inputs/Nodes_7_OutTree.dot 2 -o temp.dot -p 1 | grep "Optimal length found:"
+echo 28
+java -jar ./target/revilo-task-scheduler-1.0-SNAPSHOT.jar ./test_inputs/Nodes_7_OutTree.dot 4 -o temp.dot -p 1 | grep "Optimal length found:"
+echo 22
+java -jar ./target/revilo-task-scheduler-1.0-SNAPSHOT.jar ./test_inputs/Nodes_11_OutTree.dot 2 -o temp.dot -p 1 | grep "Optimal length found:"
+echo 350
+java -jar ./target/revilo-task-scheduler-1.0-SNAPSHOT.jar ./test_inputs/Nodes_11_OutTree.dot 4 -o temp.dot -p 1 | grep "Optimal length found:"
+echo  227
 duration=$(( SECONDS - start ))
 printf 'took %d seconds to run\n' $duration
