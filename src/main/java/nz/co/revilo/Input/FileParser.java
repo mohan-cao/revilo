7c6e5675b3e1e6310687fa0812d5e12888da5e25
mkem114 <mkem114@aucklanduni.ac.nz>
2017-08-25 04:26:26 +1200

    FileParser Refactor
    Javadoc comments updated, added, author and version tags

--- src/main/java/nz/co/revilo/Input/FileParser.java
+++ src/main/java/nz/co/revilo/Input/FileParser.java
@@ -17,7 +17,7 @@ public abstract class FileParser {
 
     /**
      * Constructor for the class which sets the file name
-     *
+     * @author Michael Kemp
      * @param filename
      */
     public FileParser(String filename) {
@@ -25,12 +25,20 @@ public abstract class FileParser {
     }
 
     /**
+     * @author Michael Kemp
      * @return the filename of the file the FileParser is to parse/has parsed, as set in the constructor
      */
     protected String getFilename() {
         return _filename;
     }
 
+    /**
+     * If an error occurs during file reading and there is no obvious way to recover from it, gracefully quite while
+     * giving diagnostic information from the exception
+     *
+     * @param e The exception that occurred during file reading
+     * @author Michael
+     */
     protected final void fileReadingError(Exception e) {
         System.out.println("File could not be read");
         System.out.println(e.getMessage());
@@ -40,7 +48,7 @@ public abstract class FileParser {
 
     /**
      * Initiates the file parsing on another thread and returns the result to the listener
-     *
+     * @author Michael Kemp
      * @param newListener
      * @throws FileNotFoundException
      */
