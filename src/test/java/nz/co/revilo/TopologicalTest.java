722e870eac9711fe9f74d7c8a8ebcdd4d87d1c97
mohan-cao <mohancao@yahoo.com.au>
2017-08-25 19:57:14 +1200

    Initial cleanup for new testing branch.

--- src/test/java/nz/co/revilo/TopologicalTest.java
+++ src/test/java/nz/co/revilo/TopologicalTest.java
@@ -2,6 +2,7 @@ package nz.co.revilo;
 
 import nz.co.revilo.Scheduling.Topological.ImprovedTopologicalAlgorithmManager;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -15,22 +16,6 @@ import static org.junit.Assert.assertTrue;
  * @version alpha
  */
 public class TopologicalTest extends ValidityTest {
-
-    class Pair<K,V>{
-        private K _a;
-        private V _b;
-        public Pair(K a, V b){
-            _a = a;
-            _b = b;
-        }
-        @Override
-        public boolean equals(Object o){
-            if(o instanceof Pair){
-                return (this._a == ((Pair)o)._a && this._b == ((Pair)o)._b);
-            }
-            return false;
-        }
-    }
 
     /**
      * Sets up a new AlgorithmManager before each test case. AlgorithmManagerImplementations can be switched out
@@ -136,11 +121,13 @@ public class TopologicalTest extends ValidityTest {
 
     /**
      * Tests against a small branching DAG with arbitrary branching
-     * 
-     * TODO: Test accepted output currently doesn't cover all correct output
-     * @author Abby S : uncomment @Test annotation when fixed
+     * Deprecated. Does not fully work in all cases.
+     *
+     * @author Abby S
      */
-//    @Test
+    @Test
+    @Ignore
+    @Deprecated
     public void testSmallBranchingDAG(){
         TestResultListener t = schedule(AppTest.TEST_PATH + "input3.dot");
         assertTrue(satisfiesDependencies(t));
@@ -152,10 +139,10 @@ public class TopologicalTest extends ValidityTest {
             if(s.equals("0")){
                 assertEquals(starttime,0);
                 assertEquals(weight,4);
-            }else if(s.equals("1")||s.equals("2")||s.equals("3")){ //TODO
+            }else if(s.equals("1")||s.equals("2")||s.equals("3")){
                 assertTrue(starttime==4||starttime==6||starttime==8);
                 assertEquals(weight,2);
-            }else if(s.equals("4")||s.equals("5")){ //TODO
+            }else if(s.equals("4")||s.equals("5")){
                 assertTrue(starttime==10||starttime==15);
                 assertEquals(weight,5);
             }else if(s.equals("6")){
